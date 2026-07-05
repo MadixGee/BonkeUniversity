03696c88d728228d112d5713a8b6d9b923062c1a
MadixGee <bonkegcobo291195@gmail.com>
2026-07-05 18:15:39 +0200

    refactor: add functions and decisions

---
+++ projects/module-01-project/DECISIONS.md
@@ -0,0 +1,2 @@
+First choice I made is introduce to separate logic into functions.
+Good for decoupling however the developer would jump around to look through code
--- projects/module-01-project/orders.py
+++ projects/module-01-project/orders.py
@@ -1,26 +1,40 @@
-def calc(o):
-    invoiceTotal = 0
-    for i in o["items"]:
-        invoiceTotal = invoiceTotal + i["p"] * i["q"]
-    # discount
-    if invoiceTotal > 100:
-        if o["c"] == "gold":
-            invoiceTotal = invoiceTotal - invoiceTotal * 0.15
-        else:
-            invoiceTotal = invoiceTotal - invoiceTotal * 0.05
-    else:
-        if o["c"] == "gold":
-            invoiceTotal = invoiceTotal - invoiceTotal * 0.10
-    # tax
-    t = invoiceTotal + invoiceTotal  * 0.2
-    # shipping
-    if invoiceTotal < 50:
-        invoiceTotal = invoiceTotal + 9.99
-    else:
-        invoiceTotal = invoiceTotal + 0
-    return round(t, 2)
+def calc(orders):
+    sub_total = 0
+    for i in orders["items"]:
+        sub_total = sub_total + i["p"] * i["q"]
 
-# ad-hoc "tests"
-print(calc({"items":[{"p":10,"q":2},{"p":5,"q":1}], "c":"normal"}))
-print(calc({"items":[{"p":80,"q":2}], "c":"gold"}))
-print(calc({"items": [{"p": 10, "q": 2}, {"p": 5, "q": 1}], "c": "gold"}))
+    # discount
+    discounted_total = sub_total - calc_discount(orders["c"], sub_total)
+
+    # tax
+    totalAfterTax = calc_tax(discounted_total)
+
+    # shipping
+    sub_total = calc_shipping(totalAfterTax)
+
+    return round(sub_total, 2)
+
+
+def calc_tax(sub_total):
+    sub_total = sub_total + sub_total * 0.2
+    return round(sub_total, 2)
+
+
+def calc_shipping(sub_total):
+       if sub_total < 50:
+        sub_total = sub_total + 9.99
+
+       return sub_total
+
+def calc_discount(customer_type, sub_total):
+    match customer_type:
+        case "gold":
+            if sub_total > 100:
+                return sub_total * 0.15
+            else:
+                return sub_total * 0.10
+        case "normal":
+            if sub_total > 100:
+                return sub_total * 0.05
+            else:
+                return 0
